7f84d887d383220430c5e044352092dd092928ee
Zeyad Lotfy <zezoramy380@gmail.com>
2025-05-12 09:07:36 +0300

    server: add roles decorators - add auth guard - add roles guard

---
+++ server/src/common/gurads/auth.guard.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    try {
+      const payload = await this.jwt.verifyAsync(token, {
+        secret: this.config.get<string>('JWT_SECRET'),
+      });
+
+      request['user'] = payload;
+
+      return true;
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
---
+++ server/src/common/gurads/roles.guard.ts
@@ -0,0 +1,41 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { PrismaService } from '../../modules/prisma/prisma.service';
+import { ROLES_KEY } from '../decorators/roles/roles.decorator';
+import { Role } from '@prisma/client';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private prisma: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    const userFromDb = await this.prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+      select: {
+        role: true,
+      },
+    });
+
+    if (!userFromDb) {
+      return false;
+    }
+
+    return requiredRoles.includes(userFromDb.role);
+  }
+}
