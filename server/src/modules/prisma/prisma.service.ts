b22abcdbc4d2637ee0d52ea2f5a6008432206178
Zeyad Lotfy <zezoramy380@gmail.com>
2025-05-12 07:30:56 +0300

    server: add prisma setup and prisma module - connect to mongodb

---
+++ server/src/modules/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private configService: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.get<string>('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected to the database');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Disconnected from the database');
+  }
+}
